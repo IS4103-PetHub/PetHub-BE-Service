1272103aa4324cedd48546a0df40ca82f233ae05
Raine <77650951+yuyitan@users.noreply.github.com>
2023-08-26 16:44:34 +0800

    add cors config

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const createError = require('http-errors');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const createError = require("http-errors");
+const morgan = require("morgan");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+// enable cors
+const corsOptions = {
+  origin: ["http://localhost:3001", "http://localhost:3002"],
+  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.get("/", async (req, res, next) => {
+  res.send({ message: "Awesome it works 🐻" });
 });
 
-app.use('/api', require('./routes/api.route'));
+app.use("/api", require("./routes/api.route"));
 
 app.use((req, res, next) => {
   next(createError.NotFound());
