a48f88fa5be5daa862efbebd7f5315dff057b4d3
jordanwwb <e0540745@u.nus.edu>
2023-08-29 00:14:26 +0800

    fix: re-align api payload and user mngt endpoint naming
    - adjusted req payload and response payload
    - standadize path naming
    - password validation
    -

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -6,18 +6,23 @@ const prisma = new PrismaClient()
 const bcrypt = require('bcrypt');
 
 /*
-    login payload
+    login payload:
     {
         "email": "user",
         "password": "password"
     }
 
-    Returns the appropriate user information. e.g, if user is admin, return admin information.
+    returns:
+    {
+        name: "firstName",
+        role: "admin",
+        userId: 1
+    }
 */
 router.post('/login', async (req, res, next) => {
     try {
-        console.log("TEST")
-        const { email, password } = req.body;
+        const { username, password } = req.body;
+        const email = username
         const user = await prisma.user.findUnique({
             where: {
                 email,
@@ -37,11 +42,28 @@ router.post('/login', async (req, res, next) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
+
+
         if (user.applicationAdmin) {
-            return res.status(200).json(user.applicationAdmin);
+            const admin = {
+                name: user.applicationAdmin.firstName,
+                role: "applicationAdmin",
+                userId: user.userId
+            }
+            return res.status(200).json(admin);
         } else if (user.petOwner) {
-            return res.status(200).json(user.petOwner);
+            const petOwner = {
+                name: user.petOwner.firstName,
+                role: "petOwner",
+                userId: user.userId
+            }
+            return res.status(200).json(petOwner);
         } else if (user.petBusiness) {
+            const petBusiness = {
+                name: user.petBusiness.companyName,
+                role: "petBusiness",
+                userId: user.userId
+            }
             return res.status(200).json(user.petBusiness);
         } else {
             return res.status(401).json({ message: 'Invalid user type' });
