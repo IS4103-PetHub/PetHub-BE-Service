6f818f447f43cbf5cbdf785365e41e3045304b8d
jordanwwb <e0540745@u.nus.edu>
2023-08-29 16:47:54 +0800

    fix: typo on validating usertypes for auth

--- src/api/validations/auth.js
+++ src/api/validations/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     validateUserType: async (userType, user) => {
-        if (!userType || !(userType == "applicationAdmin" || userType == "petOwner" || userType == "petBusienss")) {
+        if (!userType || !(userType == "applicationAdmin" || userType == "petOwner" || userType == "petBusiness")) {
             return false
         }
         if ((userType == "applicationAdmin" && !user.applicationAdmin) ||
