73d00a3002b8e7cffbe91448f2b9a9c59f7db8bc
Ryo <ryo200013@gmail.com>
2023-09-14 12:34:54 +0800

    Improve enum validation (so wont have to modify if enum changes etcetc)

--- src/api/validations/petBusinessApplicationValidation.js
+++ src/api/validations/petBusinessApplicationValidation.js
@@ -1,32 +1,16 @@
 const { BusinessType, BusinessApplicationStatus } = require("@prisma/client");
 
-exports.isValidBusinessType = async (strData) => {
-  switch (strData) {
-    case BusinessType.FNB:
-    case BusinessType.HEALTHCARE:
-    case BusinessType.SERVICE:
-      return true;
-    default:
-      return false;
-  }
+exports.isValidBusinessType = (strData) => {
+  return Object.values(BusinessType).includes(strData);
 };
 
-exports.isValidApplicationStatus = async (strData) => {
-  switch (strData) {
-    case BusinessApplicationStatus.APPROVED:
-    case BusinessApplicationStatus.REJECTED:
-    case BusinessApplicationStatus.PENDING:
-      return true;
-    default:
-      return false;
-  }
+exports.isValidApplicationStatus = (strData) => {
+  return Object.values(BusinessApplicationStatus).includes(strData);
 };
 
 // Put in its own address module if have time
 exports.validAddressFieldsPresent = async (addresses) => {
-  // Define required fields for Address model
   const requiredAddressFields = ["addressName", "line1", "postalCode"];
-
   for (const address of addresses) {
     for (const field of requiredAddressFields) {
       if (address[field] === undefined) {
