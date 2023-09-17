993dee83b963b66b4868443da342b81016693141
lohwenxin <e0563995@u.nus.edu>
2023-09-17 16:49:16 +0800

    update migrations

--- prisma/migrations/20230915173229_migration_11/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ServiceListing" ADD COLUMN     "attatchments" TEXT[];
--- prisma/migrations/20230916090812_migration_12/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `attatchments` on the `ServiceListing` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ServiceListing" DROP COLUMN "attatchments",
-ADD COLUMN     "attachment" TEXT[];
