ce36f28130a4c7808c26981a56b78ce1ded5cd7b
Ryo <ryo200013@gmail.com>
2023-09-23 03:19:41 +0800

    PR comment: Wrap multi prisma ops in transactions
    Make AddressService into class + remove redundant else blocks in there

--- src/api/services/petBusinessApplicationService.js
+++ src/api/services/petBusinessApplicationService.js
@@ -32,44 +32,54 @@ class PetBusinessApplicationService {
           400
         );
       }
-
-      // Create addresses
-      let addressIds = [];
-      if (data.businessAddresses && data.businessAddresses.length > 0) {
-        for (let address of data.businessAddresses) {
-          const newAddress = await AddressService.createAddress(address);
-          addressIds.push(newAddress.addressId);
+      const petBusinessApplication = await prisma.$transaction(async (prismaClient) => {
+        // Create addresses
+        let addressIds = [];
+        if (data.businessAddresses && data.businessAddresses.length > 0) {
+          for (let address of data.businessAddresses) {
+            const newAddress = await prismaClient.address.create({
+              data: {
+                addressName: address.addressName,
+                line1: address.line1,
+                line2: address.line2,
+                postalCode: address.postalCode,
+              },
+            });
+            addressIds.push(newAddress.addressId);
+          }
         }
-      }
 
-      const petBusinessApplication = await prisma.petBusinessApplication.create({
-        data: {
-          businessType: data.businessType,
-          businessEmail: data.businessEmail,
-          businessDescription: data.businessDescription,
-          websiteURL: data.websiteURL,
-          attachments: data.attachments || [],
-          adminRemarks: [],
-          businessAddresses: {
-            connect: addressIds.map((id) => ({ addressId: id })),
-          },
-          petBusiness: {
-            connect: {
-              userId: Number(data.petBusinessId),
+        const petBusinessApplication = await prismaClient.petBusinessApplication.create({
+          data: {
+            businessType: data.businessType,
+            businessEmail: data.businessEmail,
+            businessDescription: data.businessDescription,
+            websiteURL: data.websiteURL,
+            attachments: data.attachments || [],
+            adminRemarks: [],
+            businessAddresses: {
+              connect: addressIds.map((id) => ({ addressId: id })),
             },
+            petBusiness: {
+              connect: {
+                userId: Number(data.petBusinessId),
+              },
+            },
+            lastUpdated: new Date(),
           },
-          lastUpdated: new Date(),
-        },
-        include: {
-          businessAddresses: true,
-        },
-      });
+          include: {
+            businessAddresses: true,
+          },
+        });
 
-      // AccountStatus: INACTIVE -> PENDING
-      await prisma.user.update({
-        where: { userId: Number(data.petBusinessId) },
-        data: { accountStatus: "PENDING" },
-      });
+        // AccountStatus: INACTIVE -> PENDING
+        await prismaClient.user.update({
+          where: { userId: Number(data.petBusinessId) },
+          data: { accountStatus: "PENDING" },
+        });
+
+        return petBusinessApplication;
+      }); // end of transaction
 
       return petBusinessApplication;
     } catch (error) {
@@ -92,35 +102,48 @@ class PetBusinessApplicationService {
         throw new CustomError("Only applications with REJECTED status can be updated by the PB", 400);
       }
 
-      // Delete the old addresses if they exist (these addresses are not linked to the pet business!)
-      for (let address of existingApplication.businessAddresses) {
-        AddressService.deleteAddress(address.addressId);
-      }
-
-      // Make addresses
-      let addressIds = [];
-      if (updatedData.businessAddresses && updatedData.businessAddresses.length > 0) {
-        for (let address of updatedData.businessAddresses) {
-          const newAddress = await AddressService.createAddress(address);
-          addressIds.push(newAddress.addressId);
+      const updatedPetBusinessApplication = await prisma.$transaction(async (prismaClient) => {
+        // Delete the old addresses if they exist (these addresses are not linked to the pet business!)
+        for (let address of existingApplication.businessAddresses) {
+          await prismaClient.address.delete({
+            where: { addressId: address.addressId },
+          });
         }
-      }
 
-      const updatedPetBusinessApplication = await prisma.petBusinessApplication.update({
-        where: { petBusinessApplicationId },
-        data: {
-          ...updatedData, // Keep all the old data (including remarks etc) except the bottom 2 fields
-          applicationStatus: "PENDING", // Status goes back to PENDING so InternalUser can go review it again
-          businessAddresses: {
-            connect: addressIds.map((id) => ({ addressId: id })),
+        // Make addresses
+        let addressIds = [];
+        if (updatedData.businessAddresses && updatedData.businessAddresses.length > 0) {
+          for (let address of updatedData.businessAddresses) {
+            const newAddress = await prismaClient.address.create({
+              data: {
+                addressName: address.addressName,
+                line1: address.line1,
+                line2: address.line2,
+                postalCode: address.postalCode,
+              },
+            });
+            addressIds.push(newAddress.addressId);
+          }
+        }
+
+        const updatedPetBusinessApplication = await prismaClient.petBusinessApplication.update({
+          where: { petBusinessApplicationId },
+          data: {
+            ...updatedData, // Keep all the old data (including remarks etc) except the bottom 2 fields
+            applicationStatus: "PENDING", // Status goes back to PENDING so InternalUser can go review it again
+            businessAddresses: {
+              connect: addressIds.map((id) => ({ addressId: id })),
+            },
+            lastUpdated: new Date(),
           },
-          lastUpdated: new Date(),
-        },
 
-        include: {
-          businessAddresses: true,
-        },
-      });
+          include: {
+            businessAddresses: true,
+          },
+        });
+
+        return updatedPetBusinessApplication;
+      }); // end of transaction
 
       return updatedPetBusinessApplication;
     } catch (error) {
@@ -225,41 +248,45 @@ class PetBusinessApplicationService {
         throw new CustomError("Pet Business does not have have accountStatus PENDING", 400);
       }
 
-      // Attempt update - PB APP - BusinessApplicationStatus: PENDING/REJECTED -> APPROVED
-      const updatedApplication = await prisma.petBusinessApplication.update({
-        where: { petBusinessApplicationId: id },
-        data: { applicationStatus: "APPROVED", approverId: approverId },
-        include: {
-          businessAddresses: true,
-          petBusiness: true,
-          approver: true,
-        },
-      });
+      await prisma.$transaction(async (prismaClient) => {
+        // Attempt update - PB APP - BusinessApplicationStatus: PENDING/REJECTED -> APPROVED
+        const updatedApplication = await prismaClient.petBusinessApplication.update({
+          where: { petBusinessApplicationId: id },
+          data: { applicationStatus: "APPROVED", approverId: approverId },
+          include: {
+            businessAddresses: true,
+            petBusiness: true,
+            approver: true,
+          },
+        });
 
-      // For each address in the PB app, also link to the PB
-      const addresses = await AddressService.getAllAddressesForPetBusinessApplication(id);
-      for (let address of addresses) {
-        await AddressService.updateAddressIdForPetBusiness(
-          address.addressId,
-          associatedPetBusinessApp.petBusiness.userId
-        );
-      }
+        // For each address in the PB app, also link to the PB
+        const addresses = await AddressService.getAllAddressesForPetBusinessApplication(id);
+        for (let address of addresses) {
+          await prismaClient.address.update({
+            where: { addressId: address.addressId },
+            data: {
+              petBusinessId: associatedPetBusinessApp.petBusiness.userId,
+            },
+          });
+        }
 
-      // Attempt update - PB and User - AccountStatus: PENDING only -> ACTIVE, ALSO UPDATE the PB with the new fields from the approved application
-      await prisma.user.update({
-        where: { userId: associatedPetBusinessApp.petBusiness.userId },
-        data: {
-          accountStatus: "ACTIVE",
-          petBusiness: {
-            update: {
-              businessType: updatedApplication.businessType,
-              websiteURL: updatedApplication.websiteURL,
-              businessDescription: updatedApplication.businessDescription,
-              businessEmail: updatedApplication.businessEmail,
+        // Attempt update - PB and User - AccountStatus: PENDING only -> ACTIVE, ALSO UPDATE the PB with the new fields from the approved application
+        await prismaClient.user.update({
+          where: { userId: associatedPetBusinessApp.petBusiness.userId },
+          data: {
+            accountStatus: "ACTIVE",
+            petBusiness: {
+              update: {
+                businessType: updatedApplication.businessType,
+                websiteURL: updatedApplication.websiteURL,
+                businessDescription: updatedApplication.businessDescription,
+                businessEmail: updatedApplication.businessEmail,
+              },
             },
           },
-        },
-      });
+        });
+      }); // end of transaction
 
       // Notify PB by email
       const name = associatedPetBusinessApp.petBusiness.companyName;
@@ -330,37 +357,6 @@ class PetBusinessApplicationService {
     } catch (error) {
       if (error instanceof CustomError) throw error;
       console.error("Error during pet business application rejection:", error);
-      throw new PetBusinessApplicationError(error);
-    }
-  };
-
-  // This function is for testing
-  deletePetBusinessApplicationByPBId = async (id) => {
-    try {
-      const petBusinessApplication = await prisma.petBusinessApplication.findUnique({
-        where: { id },
-        include: { businessAddresses: true },
-      });
-
-      // We don't wanna delete approved applications (those are tied to the PB already)
-      if (!petBusinessApplication) {
-        throw new CustomError("Pet Business Application not found", 404);
-      }
-      if (petBusinessApplication.applicationStatus === "APPROVED") {
-        throw new CustomError("Cannot delete an approved application", 400);
-      }
-
-      // Remove addresses
-      for (let address of petBusinessApplication.businessAddresses) {
-        await AddressService.deleteAddress(address.addressId);
-      }
-
-      await prisma.petBusinessApplication.delete({
-        where: { petBusinessId },
-      });
-    } catch (error) {
-      if (error instanceof CustomError) throw error;
-      console.error("Error deleting Pet Business Application", error);
       throw new PetBusinessApplicationError(error);
     }
   };
--- src/api/services/user/addressService.js
+++ src/api/services/user/addressService.js
@@ -3,168 +3,119 @@ const prisma = new PrismaClient();
 const AddressError = require("../../errors/addressError");
 const CustomError = require("../../errors/customError");
 
-// Unassociated address creation is possible
-exports.createAddress = async (data) => {
-  try {
-    return await prisma.address.create({
-      data: {
-        addressName: data.addressName,
-        line1: data.line1,
-        line2: data.line2,
-        postalCode: data.postalCode,
-      },
-    });
-  } catch (error) {
-    console.error("Error during address creation:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
+class AddressService {
+  // Unassociated address creation is possible
+  createAddress = async (data) => {
+    try {
+      return await prisma.address.create({
+        data: {
+          addressName: data.addressName,
+          line1: data.line1,
+          line2: data.line2,
+          postalCode: data.postalCode,
+        },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.error("Error during address creation:", error);
       throw new AddressError("Failed to create addresses");
     }
-  }
-};
+  };
 
-exports.getAllAddressesForPetBusinessApplication = async (petBusinessApplicationId) => {
-  try {
-    return await prisma.address.findMany({
-      where: { petBusinessApplicationId: petBusinessApplicationId },
-    });
-  } catch (error) {
-    console.error("Error fetching addresses for pet business application:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
+  getAllAddressesForPetBusinessApplication = async (petBusinessApplicationId) => {
+    try {
+      return await prisma.address.findMany({
+        where: { petBusinessApplicationId: petBusinessApplicationId },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.error("Error fetching addresses for pet business application:", error);
       throw new AddressError("Failed to fetch addresses");
     }
-  }
-};
+  };
 
-exports.getAllAddressesForPetBusiness = async (petBusinessId) => {
-  try {
-    return await prisma.address.findMany({
-      where: { petBusinessId: petBusinessId },
-    });
-  } catch (error) {
-    console.error("Error fetching addresses for pet business:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
+  getAllAddressesForPetBusiness = async (petBusinessId) => {
+    try {
+      return await prisma.address.findMany({
+        where: { petBusinessId: petBusinessId },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.error("Error fetching addresses for pet business:", error);
       throw new AddressError("Failed to fetch addresses");
     }
-  }
-};
+  };
 
-exports.updateAddressIdForPetBusiness = async (addressId, petBusinessId) => {
-  try {
-    return await prisma.address.update({
-      where: { addressId: addressId },
-      data: {
-        petBusinessId: petBusinessId,
-      },
-    });
-  } catch (error) {
-    console.error("Error updating address for pet business:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
+  updateAddressDetailsForAddressWithAddressId = async (address) => {
+    try {
+      return await prisma.address.update({
+        where: { addressId: address.addressId },
+        data: {
+          addressName: address.addressName,
+          line1: address.line1,
+          line2: address.line2,
+          postalCode: address.postalCode,
+        },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.error("Error updating address:", error);
       throw new AddressError("Failed to update address");
     }
-  }
-};
+  };
 
-const updateAddressDetailsForAddressWithAddressId = async (address) => {
-  try {
-    return await prisma.address.update({
-      where: { addressId: address.addressId },
-      data: {
-        addressName: address.addressName,
-        line1: address.line1,
-        line2: address.line2,
-        postalCode: address.postalCode,
-      },
-    });
-  } catch (error) {
-    console.error("Error updating address:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
-      throw new AddressError("Failed to update address");
-    }
-  }
-};
-
-/* 
-  This function is being used internally by petBusinessApplicationService
-  If desired, create a new function called unlinkAddressFromPetBusiness if the intention is only to unlink from PBs while keeping the address OR
-  modify this function to do a check
-*/
-exports.deleteAddress = async (addressId) => {
-  try {
-    await prisma.address.delete({
-      where: { addressId: addressId },
-    });
-  } catch (error) {
-    console.error("Error deleting address:", error);
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
-      throw new AddressError("Failed to delete addresses");
-    }
-  }
-};
-
-/*
+  /*
   For all the address objects in businessAddresses:
     1. If it has an ID, check PB account if there is an address with that ID linked to it, these references will be kept, just the fields updated if neccessary.
     2. For all other addresses linked to the PB account, remove them.
     3. For the addresses provided with no IDs, these are new addresses. Create them.
   Return a list of IDs of new addresses to link to the PB and a list of IDs of existing addresses to delete from the PB
 */
-exports.getUpdateAddressDetailsFromAddressArray = async (userId, businessAddresses) => {
-  try {
-    let addressIds = [];
-    let existingAddressIds = [];
+  getUpdateAddressDetailsFromAddressArray = async (userId, businessAddresses) => {
+    try {
+      let addressIds = [];
+      let existingAddressIds = [];
 
-    if (businessAddresses && businessAddresses.length > 0) {
-      for (let address of businessAddresses) {
-        // If the address has an ID, update its fields and keep its linkages
-        if (address.addressId) {
-          await updateAddressDetailsForAddressWithAddressId(address);
-          existingAddressIds.push(address.addressId);
-        } else {
-          // If the address does not have an ID, create it
-          const newAddress = await exports.createAddress(address);
-          addressIds.push(newAddress.addressId);
+      if (businessAddresses && businessAddresses.length > 0) {
+        for (let address of businessAddresses) {
+          // If the address has an ID, update its fields and keep its linkages
+          if (address.addressId) {
+            await this.updateAddressDetailsForAddressWithAddressId(address);
+            existingAddressIds.push(address.addressId);
+          } else {
+            // If the address does not have an ID, create it
+            const newAddress = await this.createAddress(address);
+            addressIds.push(newAddress.addressId);
+          }
         }
       }
-    }
 
-    const currentUser = await prisma.petBusiness.findUnique({
-      where: { userId },
-      select: {
-        businessAddresses: true,
-      },
-    });
+      const currentUser = await prisma.petBusiness.findUnique({
+        where: { userId },
+        select: {
+          businessAddresses: true,
+        },
+      });
 
-    const disconnectAddresses = currentUser.businessAddresses
-      .filter((addr) => !existingAddressIds.includes(addr.addressId))
-      .map((addr) => ({ addressId: addr.addressId }));
+      const disconnectAddresses = currentUser.businessAddresses
+        .filter((addr) => !existingAddressIds.includes(addr.addressId))
+        .map((addr) => ({ addressId: addr.addressId }));
 
-    return {
-      newAddressIds: addressIds,
-      disconnectAddresses,
-    };
-  } catch (error) {
-    console.error(
-      "Failed to perform {AddressService: getUpdateAddressDetailsFromAddressArray} when updating a PB's addresses:",
-      error
-    );
-    if (error instanceof CustomError) {
-      throw error;
-    } else {
+      return {
+        newAddressIds: addressIds,
+        disconnectAddresses,
+      };
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.error(
+        "Failed to perform {AddressService: getUpdateAddressDetailsFromAddressArray} when updating a PB's addresses:",
+        error
+      );
       throw new AddressError(
         "Failed to perform {AddressService: getUpdateAddressDetailsFromAddressArray} when updating a PB's addresses"
       );
     }
-  }
-};
+  };
+}
+
+module.exports = new AddressService();
