b8651e734211204c781148bd65bb8de18567fdb2
jordanwwb <e0540745@u.nus.edu>
2023-10-19 14:14:15 +0800

    fix: ease validation for title and descriptions
    - CG
    - PL&F
    - Article

--- src/api/validations/articleValidation.js
+++ src/api/validations/articleValidation.js
@@ -10,13 +10,11 @@ exports.validateCreateAndUpdateArticlePayload = (payload) => {
     const schema = Joi.object({
         title: Joi.string()
             .trim()
-            .pattern(/^[a-zA-Z0-9\s.,:'?!]+$/, 'Title pattern')
             .min(1)
             .max(100)
             .required()
             .messages({
                 'string.empty': 'Title must not be empty.',
-                'string.pattern.base': 'Title must have a valid format.',
                 'string.min': 'Title must contain at least one character.',
                 'string.max': 'Title has a maximum of one hundred characters.'
             }),
