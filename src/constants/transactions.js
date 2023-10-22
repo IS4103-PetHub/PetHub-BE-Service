10bbf64e4a0d192338b26bbf4115cdd5b64a7a06
Choo Wei Xiang <chooweixiang98@gmail.com>
2023-10-22 22:44:06 +0800

    fix comments

--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -1,5 +1,5 @@
 const DEFAULT_CR_ID = 1; // Default Commission Rule is Seeded to ID 1
-const MISC_CHARGE_PCT = 0.07; // Default Commission Rule is Seeded to ID 1
+const MISC_CHARGE_PCT = 0.07; // Default miscellaneous charge is 7%
 const HOLDING_PERIOD = 7; // Default 7 days holding period for refund
 const PETHUB_STRIPE_HOLDING_ACCT_EMAIL = 'pethub@gmail.com'; // Pethub's holding account for holding and payout funds
 
